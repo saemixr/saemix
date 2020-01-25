3857ce32ddc7f514ece6dd2f0b7663b6c553a6d1
BelhalK <belhal.karimi@gmail.com>
2020-01-25 18:52:34 +0100

    init

---
+++ R/zzz.R
@@ -0,0 +1,3 @@
+.onAttach <-function (lib, pkg) {
+  packageStartupMessage("     Loading package saemix, version 3.1, September 2019\n         please direct bugs, questions and feedback to belhal.karimi@inria.fr\n")
+}
